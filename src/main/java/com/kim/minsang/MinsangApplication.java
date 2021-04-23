8f8edcbaa35d4094bf50bbd32ab3ec943b6865c4
kimminsang <kimminsang713@gmail.com>
2021-04-23 12:19:15 +0900

    git flow tutorial

---
+++ src/main/java/com/kim/minsang/MinsangApplication.java
@@ -0,0 +1,14 @@
+package com.kim.minsang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinsangApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MinsangApplication.class, args);
+    }
+
+}
