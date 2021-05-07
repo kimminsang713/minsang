fc3b33602d5d93279116da1255a217d864b27c3b
kimminsang <kimminsang713@gmail.com>
2021-05-07 17:11:58 +0900

    계산기 만들기 추가

--- src/main/java/com/kim/minsang/Calculator.java
+++ src/main/java/com/kim/minsang/Calculator.java
@@ -1,21 +1,36 @@
 package com.kim.minsang;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
-
+//    요구사항
+//    String으로 된 문자열을 받는다 o
+//    덧셈을 해서 결과값을 반환해주는 함수를 구현한다.
+//    이때, 숫자와 숫자를 구분할때는 , 로 구분
+//    음수가 들어올수 없다. 따라서 결과값은 무조건 양수이다.
+//    예 )
+//            1. "1" => 1
+//            2. "1,2" => 3
+//            3. "1,2,3" => 6
+//            4. "1, 2, 3" => 6
+//            6. "1, 1 2 3 4, 5," => 1240
+//            7. ",,,1,2" => 3
+//            8. "" =>
     public static void caluclator(){
+        // 값을 받을 변수 선언
+        String num = "";
+
         Scanner scan = new Scanner(System.in);
+//       // 포문으로 값을 받아 저장하기
+//        for(int i=0; i<=2; i++){
+//            System.out.print("숫자 입력 :");
+//            num = scan.nextLine();
+//        }
+        System.out.print(num);
 
-        String num1;
-        String num2;
 
-        System.out.print("숫자 1번값 : ");
-        num1= scan.nextLine();
-        System.out.print("숫자 2번값 : ");
-        num2= scan.nextLine();
-
-        System.out.println();
+        // 더하기
 
 
     }
