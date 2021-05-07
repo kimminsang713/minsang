9e3d99fe44a6674c914f56064e2dbc72d39d91d7
kimminsang <kimminsang713@gmail.com>
2021-05-08 01:58:32 +0900

    계산기 완성

--- src/main/java/com/kim/minsang/Calculator.java
+++ src/main/java/com/kim/minsang/Calculator.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class Calculator {
 //    요구사항
-//    String으로 된 문자열을 받는다 o
-//    덧셈을 해서 결과값을 반환해주는 함수를 구현한다.
-//    이때, 숫자와 숫자를 구분할때는 , 로 구분
-//    음수가 들어올수 없다. 따라서 결과값은 무조건 양수이다.
+//    1.String으로 된 문자열을 받는다 o -> 음수가 들어올수 없다.음수가 들어올수없으러면 입력받을때 체크하고 리턱시켜야행
+//    2.덧셈을 해서 결과값을 반환해주는 함수를 구현한다. o
+//    3.이때, 숫자와 숫자를 구분할때는 , 로 구분
+//    4.음수가 들어올수 없다. 따라서 결과값은 무조건 양수이다.
 //    예 )
 //            1. "1" => 1
 //            2. "1,2" => 3
@@ -22,15 +22,34 @@ public class Calculator {
         String num = "";
         String str = "";
         Scanner scan = new Scanner(System.in);
-       // 포문으로 값을 사용자한테 받아 저장하기
-        for(int i=0; i<=2; i++){
+
+        // 사용자가 몇번 입력할지 정함
+        System.out.print("몇번 입력하시겠습니까? : ");
+        int size = scan.nextInt();
+        scan.nextLine();
+
+        int i = 0;
+        // 반복문으로
+        while(i<size){ // 3번돌면 반복문 빠져나가게
             System.out.print("숫자 입력 : ");
             num = scan.nextLine();
+            if(Integer.valueOf(num) < 0){ // 입력받은 값이 음수인지 체크
+                System.out.print("음수는 입력할 수 없습니다. 다시 입력해주세요 ");
+                i--;
+            }
             str += num+",";
+            i++;
         }
-        System.out.print(str);
 
-        // 더하기
+        // split을 통해 ,로 구분지어 배열에 담기
+        String arr[] = str.split(",");
+        int result = 0;
+//        배열에서 포문으로 값을 빼온뒤 더하기
+        for(int j=0; j<arr.length; j++){
+             result+=Integer.valueOf(arr[j]);
+        }
+        //결과
+        System.out.println(arr + "," + "=>" + result);
 
 
     }
