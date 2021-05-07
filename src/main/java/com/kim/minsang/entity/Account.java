26c6e81aa79ef566ec94d42bef22c5d92c7a00aa
kimminsang <kimminsang713@gmail.com>
2021-05-07 09:46:59 +0900

    tutorial setting

--- src/main/java/com/kim/minsang/entity/Account.java
+++
@@ -1,4 +0,0 @@
-package com.kim.minsang.entity;
-
-public class Account {
-}
--- src/main/java/com/kim/minsang/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kim.minsang.repository;
-
-
-import com.kim.minsang.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Post,Long> {
-
-}
